3c5a66763b64603c988ac9020314c458c921f205
denkanikotabazaar-create <denkanikotabazaar@gmail.com>
2026-07-30 15:56:29 +0530

    Create Profile.jsx

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#ffffff",
+        padding: "20px",
+        borderRadius: "15px",
+      }}
+    >
+      <h2>👤 Profile</h2>
+
+      <p>Shop Name: Denkanikota Bazaar</p>
+
+      <p>Owner Name: Seller</p>
+
+      <p>Phone: 9876543210</p>
+
+      <p>Location: Denkanikota</p>
+    </div>
+  );
+}
