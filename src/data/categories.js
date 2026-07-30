c63bde674d9fe3600e5f4ea89e518486b8ada090
denkanikotabazaar-create <denkanikotabazaar@gmail.com>
2026-07-30 14:46:21 +0530

    Create categories.js

---
+++ src/data/categories.js
@@ -0,0 +1,12 @@
+export const categories = [
+  "Vegetables",
+  "Fruits",
+  "Flowers",
+  "Grocery",
+  "Electronics",
+  "Hardware",
+  "Salon",
+  "Beauty Parlour",
+  "Transport",
+  "Medical Shop"
+];
